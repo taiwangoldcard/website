510e19152a1cbe9b78e63cf6455aea541afff460
nat236919 <nuttaphat.aruno@gmail.com>
2020-07-21 17:24:50 +0800

    (#64) limit latestNews content display

---
+++ content/news.md
@@ -0,0 +1,7 @@
+## Gold Card News
+
+- <time datetime="2020-06-30">2020-06-30</time> [Almost 900 gold cards issued!](https://foreigntalentact.ndc.gov.tw/en/News_Content.aspx?n=F0746484B877D582&s=91B121FE3FA7C24D)
+- <time datetime="2020-06-12">2020-06-12</time> [President Tsai Promotes Gold Card](https://english.president.gov.tw/News/6008)
+- <time datetime="2020-06-05">2020-06-05</time> [Goldcard holder: Tomáš řízek illustration exhibition](https://99dac.com/exhibition-detail.php?id=140)
+- <time datetime="2020-06-05">2020-05-27</time> [Goldcard holder: Starting an eye yoga career](https://meet.bnext.com.tw/intl/articles/view/46488)
+- <time datetime="2020-06-05">2020-04-13</time> [Tax payment has been extended to June 30th 2020](https://home.kpmg/us/en/home/insights/2020/04/tnf-taiwan-tax-return-tax-payment-deadlines-extended-covid-19.html)
---
+++ static/js/news_display.js
@@ -0,0 +1,38 @@
+// JS for displaying latestNews
+$(document).ready(function() {
+    var limitContent = 8;
+    var selectorName = ".latestNews";
+    var filteredNewsList = filterNewsList(selectorName, limitContent);
+    var ulId = "lastestNewsList";
+    displayNewsList(selectorName, filteredNewsList, ulId, function() {
+        $("#" + ulId).append('<a href="news" target="_blank" rel="noopener">Read More</a>');
+    });
+})
+
+// Select the first n of news list
+function filterNewsList(selectorName, limitContent) {
+    if (typeof limitContent !== "number" || limitContent === null)
+        return;
+
+    let filteredNewsList = [];
+    $(selectorName).html(function() {
+        let li = $(this).find("li");
+        $(li).each(function(index, value){
+            if (index < limitContent) filteredNewsList.push(value);
+        })
+    });
+
+    return filteredNewsList;
+}
+
+// Display news list
+function displayNewsList(selectorName, newsList, ulId, objCallBack) {
+    $(selectorName + " > ul").attr("id", ulId); // add id to unordered list
+    $("#" + ulId).empty(); // clear old ul
+    $.each(newsList, function(key, value) {
+        $("#" + ulId).append(value);
+    });
+
+    if (typeof objCallBack !== "undefined" && objCallBack !== null)
+        objCallBack();
+}
