f323e881e746f3db4ffee3e7ed1c9bc83afe72cd
Shawn Lim <shawnlim89@live.com>
2020-09-12 17:51:09 +0800

    Add test navigation for home page links

--- cypress/integration/website/index.js
+++ cypress/integration/website/index.js
@@ -1,6 +1,21 @@
 describe("Home page", () => {
-  it("Banner exists", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:1313");
+  });
+
+  it("Banner exists", () => {
     cy.get(".section.homePicture img");
+  });
+
+  it("Can navigate to applicant FAQ", () => {
+    cy.scrollTo(0, 500);
+    cy.contains("Access the FAQ").click({ force: true });
+    cy.contains("Gold Card Application FAQ");
+  });
+
+  it("Can navigate to goldcard holders FAQ", () => {
+    cy.scrollTo(0, 500);
+    cy.contains("Resources for GoldCard holders").click({ force: true });
+    cy.contains("Gold Card Holders FAQ");
   });
 });
