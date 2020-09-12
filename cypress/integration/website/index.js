37c21d0d4e9207454dab78be5955141812327b30
Shawn Lim <shawnlim89@live.com>
2020-09-12 17:54:59 +0800

    Add navigation to news page

--- cypress/integration/website/index.js
+++ cypress/integration/website/index.js
@@ -7,6 +7,11 @@ describe("Home page", () => {
     cy.get(".section.homePicture img");
   });
 
+  it("Can navigate to news page", () => {
+    cy.get("#lastestNewsList").contains("Read More").click({ force: true });
+    cy.get(".content").contains("Gold Card News");
+  });
+
   it("Can navigate to applicant FAQ", () => {
     cy.scrollTo(0, 500);
     cy.contains("Access the FAQ").click({ force: true });
